abeb78ca32a3082095ec586fbcff4cb4db63247f
Sepehr <bizhan.he1996@gmail.com>
2025-08-03 23:28:57 +0330

    Injected authorization data into layout

---
+++ public/js/document.js
@@ -0,0 +1,43 @@
+﻿window.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("div.tag-selector input").forEach((input) => {
+    input.addEventListener("keydown", (event) => {
+      const input = event.target;
+      let query;
+      if (event.key === "Enter") {
+        event.preventDefault();
+        query = input.value;
+        const matches = [...input.nextElementSibling.children].filter(
+          (option) => {
+            return option.label.toLowerCase().includes(query.toLowerCase());
+          }
+        );
+        if (matches.length === 1) {
+          matches[0].selected = true;
+          const selectedTagNode = document.createElement("span");
+          selectedTagNode.classList.add("tag");
+          selectedTagNode.innerHTML = matches[0].label;
+          input.previousElementSibling.append(selectedTagNode);
+          selectedTagNode.addEventListener("click", (event) => {
+            event.target.parentNode.parentNode.querySelector(
+              `select option[label=${selectedTagNode.innerHTML}]`
+            ).selected = false;
+            event.target.remove();
+          });
+          input.value = null;
+        }
+      } else if (event.key == "Backspace") {
+        const candidate = [...input.previousElementSibling.children]?.at(-1);
+        if (candidate) {
+          input.parentNode.querySelector(
+            `select option[label=${candidate.innerHTML}]`
+          ).selected = false;
+          candidate.remove();
+        }
+      }
+    });
+  });
+
+  document.querySelectorAll("select#pagination-size").forEach((select) => {
+    select.value = window.localStorage.getItem("pagination-size");
+  });
+});
---
+++ public/js/functions.js
@@ -0,0 +1,135 @@
+export  const toggleTheme = (elm) => {
+  const theme = document.querySelector("html").classList.item(1);
+  const html = document.querySelector("html");
+  html.classList = ["panel"];
+  if (theme == "light") {
+    html.classList.add("dark");
+    elm.children[0].innerHTML = "light_mode";
+    elm.children[1].innerHTML = "Light";
+  } else {
+    html.classList.add("light");
+    elm.children[0].innerHTML = "dark_mode";
+    elm.children[1].innerHTML = "Dark";
+  }
+};
+
+export const injectModuleHelpData = (moduleHelpData) => {
+  const moduleHelp = document.querySelector("#module-help");
+  moduleHelp.querySelector("#module-help-title").innerHTML = moduleHelpData.title;
+  // foreach role
+  for (const role of ["Admin", "Manager", "Client"]) {
+    const tr = document.createElement("tr");
+    const td = document.createElement("td");
+    td.innerHTML = role;
+    tr.appendChild(td);
+    if (moduleHelpData["permissions"][role] === false) {
+      for (let i in [0, 1, 2, 3]) {
+        const emptyTd = document.createElement("td");
+        tr.appendChild(emptyTd);
+      }
+    } else {
+      // for each permission
+      for (const [permission, value] of Object.entries(
+        moduleHelpData["permissions"][role]
+      )) {
+        const td = document.createElement("td");
+        if (value) {
+          td.innerHTML = "✓";
+        }
+        tr.appendChild(td);
+      }
+    }
+    moduleHelp.querySelector("tbody").appendChild(tr);
+  }
+  moduleHelp.querySelector("p#module-help-description").innerHTML =
+    moduleHelpData.description;
+};
+
+export const toggleModuleHelp = (target) => {
+  // variables
+  const blackOverlay = document.querySelector("#black-overlay");
+  const moduleHelp = document.querySelector("#module-help");
+  const action = moduleHelp.classList.contains("-translate-x-full")
+    ? "hide"
+    : "show";
+  if (action == "show") {
+    blackOverlay.addEventListener("click", toggleModuleHelp);
+    moduleHelp.classList.add("-translate-x-full");
+  } else if (action == "hide") {
+    blackOverlay.removeEventListener("click", toggleModuleHelp);
+    moduleHelp.classList.remove("-translate-x-full");
+  }
+  toggleBlackOverlay();
+};
+
+export const toggleAside = (target) => {
+  target.parentNode.parentNode.parentNode.parentNode.classList.toggle(
+    "aside-toggle"
+  );
+  target.children[0].classList.toggle("rotate-180");
+};
+
+export const toggleDirection = (element) => {
+  const html = document.querySelector("html");
+  const direction = html.getAttribute("dir");
+  if (direction == "rtl") {
+    html.setAttribute("dir", "ltr");
+    element.children[0].innerHTML = "format_textdirection_r_to_l";
+    element.children[1].innerHTML = "RTL";
+  } else {
+    html.setAttribute("dir", "rtl");
+    element.children[0].innerHTML = "format_textdirection_l_to_r";
+    element.children[1].innerHTML = "LTR";
+  }
+};
+
+export const toggleAsideMobile = async () => {
+  const aside = document.querySelector("aside");
+  const leftValue = aside.style.marginInlineStart;
+  const action = leftValue == "0px" ? "hide" : "show";
+  if (action == "show") {
+    document
+      .querySelector("#black-overlay")
+      .addEventListener("click", toggleAsideMobile);
+  } else if (action == "hide") {
+    document
+      .querySelector("#black-overlay")
+      .removeEventListener("click", toggleAsideMobile);
+  }
+  aside.style.marginInlineStart = action == "show" ? "0" : "-100%";
+  toggleBlackOverlay();
+};
+
+export const toggleBlackOverlay = () => {
+  const blackOverlay = document.querySelector("#black-overlay");
+  const action = blackOverlay.classList.contains("block") ? "hide" : "show";
+  if (action === "show") {
+    blackOverlay.classList.add("block");
+    window.setTimeout(() => {
+      blackOverlay.classList.add("opacity-100");
+    }, 1);
+  } else if (action === "hide") {
+    blackOverlay.classList.remove("opacity-100");
+    window.setTimeout(() => {
+      blackOverlay.classList.remove("block");
+    }, 300);
+  }
+};
+
+export const toggleSubmenu = (elm) => {
+  const height = elm.nextElementSibling.style.height;
+  elm.nextElementSibling.style.height = height == "auto" ? "0" : "auto";
+  [...elm.children].at(-1).classList.toggle("rotate-180");
+};
+
+export const toggleConfirm = (message, elm) => {
+  if (window.confirm(message)) {
+    elm.querySelector("a").click();
+  }
+};
+
+export const changePageSize = (controller, selectElement) => {
+  window.localStorage.setItem("pagination-size", selectElement.value);
+  window.location.href =
+    "/" + controller + "?page=1&size=" + selectElement.value;
+};
